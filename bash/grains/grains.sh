59f8bdced30a3af44e0ee6913cce6057b4aed674
Josh McArthur <joshua.mcarthur@gmail.com>
2018-11-03 22:57:00 +1300

    Implement handling for 'total'

--- bash/grains/grains.sh
+++ bash/grains/grains.sh
@@ -1,16 +1,36 @@
 #!/usr/bin/env bash
 
+calc() {
+  local field=$1
+  echo "$(( 2**(${field}-1)))"
+}
+
+total() {
+  local sum=0
+  for i in {1..64} 
+  do
+    sum=$(($sum+$(calc $i)))
+  done
+
+  printf "%u\n" $sum
+}
+
 main() {
   local field=$1
   local min=1
   local max=64
+
+  if [ "$field" = "total" ]; then
+    total
+    exit
+  fi
 
   if [ "$field" -lt "$min" -o "$field" -gt "$max" ]; then
     echo "Error: invalid input"
     exit 1
   fi
 
-  local result=$((2**(${field}-1)))
+  local result=$(calc $field)
   printf "%u\n" $result
 }
 
