268a3fcf5364b9c0998155d95b3fd19bb692ae24
Josh McArthur <joshua.mcarthur@gmail.com>
2018-11-24 21:13:49 +1300

    Add Elixir word count exercise

---
+++ elixir/word-count/word_count.exs
@@ -0,0 +1,26 @@
+defmodule Words do
+  @doc """
+  Count the number of words in the sentence.
+
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t()) :: map
+  def count(sentence) do
+    word_matcher = ~r/((?:\p{L}|-|[0-9])+)/iu
+    words = Regex.scan(word_matcher ,sentence, capture: :all_but_first) 
+            |> List.flatten 
+    count_words(words)
+  end
+
+  defp count_words(words, counts \\ %{}) 
+
+  defp count_words([word | words], counts) do
+    word = String.downcase(word)
+    counts = Map.update(counts, word, 1, &(&1 + 1))
+    count_words(words, counts)
+  end
+
+  defp count_words([], counts) do
+    counts
+  end
+end
